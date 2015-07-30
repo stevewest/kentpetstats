7e70c7cf2304f01e7d7641a8b6c834605f7b5b4f
Steve "uru" West <uruwolf@gmail.com>
2015-07-30 21:46:20 +0100

    Fixes wrong image name name and makes first pet selected by default.

--- application/views/pets/index.php
+++ application/views/pets/index.php
@@ -2,27 +2,31 @@
 
 	<!-- Nav tabs -->
 	<ul class="nav nav-tabs" role="tablist">
+		<?php $first = true; ?>
 		<?php foreach ($owner['pets'] as $pet): ?>
-		<li role="presentation">
+		<li role="presentation" class="<?php echo ($first)?'active':'';?>">
 			<a href="#pet_<?php echo $pet['_id']; ?>"
 			   role="tab"
 			   data-toggle="tab">
 				<?php echo $pet['name']; ?>
 			</a>
 		</li>
+			<?php $first = false; ?>
 		<?php endforeach; ?>
 	</ul>
 
 	<!-- Tab panes -->
 	<div class="tab-content">
+		<?php $first = true; ?>
 		<?php foreach ($owner['pets'] as $pet): ?>
 			<div role="tabpanel"
-				 class="tab-pane"
+				 class="tab-pane <?php echo ($first)?'active':'';?>"
 				 id="pet_<?php echo $pet['_id']; ?>"
 				 aria-controls="pet_<?php echo $pet['_id']; ?>"
 				>
 				<?php echo View::forge('pets/info', $pet) ?>
 			</div>
+			<?php $first = false; ?>
 		<?php endforeach; ?>
 	</div>
 
